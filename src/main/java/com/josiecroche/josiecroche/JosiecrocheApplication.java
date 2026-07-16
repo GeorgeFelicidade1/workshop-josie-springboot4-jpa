818bd30b920e889c8dd6a5b18c47e5496f4005e2
George <georgefabrizio@hotmail.com>
2026-07-16 18:58:18 -0300

    Projeto criado

---
+++ src/main/java/com/josiecroche/josiecroche/JosiecrocheApplication.java
@@ -0,0 +1,13 @@
+package com.josiecroche.josiecroche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JosiecrocheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JosiecrocheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josiecroche/josiecroche/JosiecrocheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josiecroche.josiecroche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JosiecrocheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
